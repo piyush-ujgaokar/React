e5e8a485bcfcfcb8361ebdd43c04919ee1608aaf
piyush ujgaokar <piyushujgaokar544@gmail.com>
2025-10-11 20:56:21 +0530

    Components Practice done

---
+++ practice1-component/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+        <div className="card">
+            <h1>Welcome </h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, magni.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
---
+++ practice1-component/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+            <h1>Logo</h1>
+            <div className="right">
+                <h3>Home</h3>
+                <h3>Products</h3>
+                <h3>About</h3>
+                <h3>Contact</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
